e94b684f3e327484a6904514377633b3d3c36055
dongjianxiong <jianxiong20090919@126.com>
2022-12-15 20:04:16 +0800

    Update mudule

--- Example/Pods/LNAccountModule/LNAccountModule/Classes/LNAccountModule.h
+++
@@ -1,8 +0,0 @@
-//
-//  LNLogin.h
-//  LNModuleCore
-//
-//  Created by Lenny on 2020/7/1.
-//
-
-#import "LNLoginManager.h"
--- Example/Pods/Target Support Files/LNAccountModule/LNAccountModule-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LNAccountModule : NSObject
-@end
-@implementation PodsDummy_LNAccountModule
-@end
